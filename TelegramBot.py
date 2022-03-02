9c0f9d63751fa85b9ccf9d2d8898a3438dde8efa
mafgomes <mafgomes@senado.leg.br>
2022-03-02 03:22:16 -0300

    Isolamento da chave da API do restante do código

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,8 +1,7 @@
 import telebot
+import BotCredentials
 
-api_key = "SUA CHAVE DA API"               # API KEY do bot
-
-bot = telebot.TeleBot(api_key)
+bot = telebot.TeleBot(botCredentials.api_key)
 
 @bot.message_handler('/start')             # Define o comando que o bot ira responder;
 def comando(mensagem):                     # Criando uma funcao para a resposta;
---
+++ botCredentials.py
@@ -0,0 +1,1 @@
+api_key = "SUA CHAVE DA API"               # API KEY do bot
